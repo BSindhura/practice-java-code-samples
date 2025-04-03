4a7b055731a647b1236ec12f0dc8f3b01fe1f407
BSindhura <sindhubattula0@gmail.com>
2025-04-03 04:38:42 -0400

    Code

---
+++ src/com/practice/java/code/samples/javafundmentals/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package com.practice.java.code.samples.javafundmentals;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		
+		//Defines and stores a student's ID as an integer, 
+		//score as a double, grade as a character, and 
+		//active status as a boolean.
+		
+		int studentId = 101;
+		double studentScore = 95.7;
+		char grade = 'A';
+		boolean isActive = true;
+		
+		System.out.println("Student Dd: " +studentId );
+		System.out.println("Student Score: " +studentScore);
+		System.out.println("Grade: " +grade);
+		System.out.println("Active Status: " +isActive);
+
+	}
+
+}
